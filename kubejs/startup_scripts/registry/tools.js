88ba583b99c9c0f724b4d16d3db8c63482bfb78f
KCtops6 <kctops06@gmail.com>
2025-03-10 22:17:44 -0400

    Added Infinite Horizons Stone Hammer (for early cobbleworks).

--- kubejs/startup_scripts/registry/tools.js
+++ kubejs/startup_scripts/registry/tools.js
@@ -2,4 +2,6 @@
 StartupEvents.registry('item', event => {
     event.create('wooden_trowel', 'shovel').tier('wood')
     event.create('stone_trowel', 'shovel').tier('stone')
+
+    event.create('stone_hammer', 'pickaxe').tier('stone')
 })

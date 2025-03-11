0365d8340721c81dafcf8419809be290bcb0b02b
KCtops6 <kctops06@gmail.com>
2025-03-10 23:45:45 -0400

    Added Iron Trowel.

--- kubejs/startup_scripts/registry/tools.js
+++ kubejs/startup_scripts/registry/tools.js
@@ -1,7 +1,7 @@
-// Listen to item registry event
 StartupEvents.registry('item', event => {
     event.create('wooden_trowel', 'shovel').tier('wood')
     event.create('stone_trowel', 'shovel').tier('stone')
+    event.create('iron_trowel', 'shovel').tier('iron')
 
     event.create('stone_hammer', 'pickaxe').tier('stone')
 })

a4b9343c14a03fd96d1978f453df5e5a21abc05e
KCtops6 <kctops06@gmail.com>
2025-03-10 22:20:17 -0400

    renamed pebbles.js to trowels.js.

--- kubejs/server_scripts/normal mode/LootJS/modifiers/block/trowels.js
+++ kubejs/server_scripts/normal mode/LootJS/modifiers/block/trowels.js
@@ -5,4 +5,6 @@ LootJS.modifiers(event => {
     event.addTableModifier("minecraft:blocks/grass_block").addLoot(LootEntry.of("kubejs:stone_pebble").randomChance(0.5)).matchMainHand("kubejs:stone_trowel")
     event.addTableModifier("minecraft:blocks/dirt").addLoot(LootEntry.of("kubejs:andesite_pebble").randomChance(0.25)).matchMainHand("kubejs:stone_trowel")
     event.addTableModifier("minecraft:blocks/grass_block").addLoot(LootEntry.of("kubejs:andesite_pebble").randomChance(0.25)).matchMainHand("kubejs:stone_trowel")
+    
+    event.addTableModifier("minecraft:blocks/gravel").addLoot(LootEntry.of('create:crushed_raw_iron').randomChance(0.25)).matchMainHand("kubejs:stone_trowel")
 })

c6a2d9aa5b26675de93cd510f244e659961cf375
KCtops6 <kctops06@gmail.com>
2025-03-11 22:37:43 -0400

    added tuff and deepslate gravel hammering recipes.

--- kubejs/server_scripts/normal mode/LootJS/modifiers/block/hammers.js
+++ kubejs/server_scripts/normal mode/LootJS/modifiers/block/hammers.js
@@ -1,4 +1,6 @@
 LootJS.modifiers(event => {
     event.addTableModifier("minecraft:blocks/cobblestone").removeLoot("minecraft:cobblestone").addLoot(LootEntry.of("minecraft:gravel")).matchMainHand('kubejs:stone_hammer')
     event.addTableModifier("minecraft:blocks/gravel").removeLoot("minecraft:gravel").addLoot(LootEntry.of("minecraft:sand")).matchMainHand('kubejs:stone_hammer')
+    event.addTableModifier("minecraft:blocks/tuff").removeLoot("minecraft:tuff").addLoot(LootEntry.of("kubejs:tuff_gravel")).matchMainHand('kubejs:stone_hammer')
+    event.addTableModifier("minecraft:blocks/cobbled_deepslate").removeLoot("minecraft:cobbled_deepslate").addLoot(LootEntry.of("kubejs:deepslate_gravel")).matchMainHand('kubejs:stone_hammer')
 })

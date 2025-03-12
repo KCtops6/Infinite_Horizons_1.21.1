cdc9e605349955cd602ccc2799edb0e1a0f9157f
KCtops6 <kctops06@gmail.com>
2025-03-11 22:24:19 -0400

    added recipe for cobbled deepslate from deepslate pebbles.

--- kubejs/server_scripts/normal mode/recipes/shapeless/two_by_two.js
+++ kubejs/server_scripts/normal mode/recipes/shapeless/two_by_two.js
@@ -1,7 +1,8 @@
 ServerEvents.recipes(event => {
     const mappings = [
         { input: 'kubejs:stone_pebble', output: 'minecraft:cobblestone'},
-        { input: 'kubejs:andesite_pebble', output: 'minecraft:andesite'}
+        { input: 'kubejs:andesite_pebble', output: 'minecraft:andesite'},
+        { input: 'kubejs:deepslate_pebble', output: 'minecraft:cobbled_deepslate'}
     ]
     mappings.forEach(recipe => {
         event.shapeless(recipe.output, Item.of(recipe.input, 4))

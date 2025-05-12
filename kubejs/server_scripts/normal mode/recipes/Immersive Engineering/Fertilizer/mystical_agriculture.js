16be8b1a840f20fe88e435769d3340376adb82e7
KCtops6 <kctops06@gmail.com>
2025-05-12 09:36:24 -0400

    Added Mystical Agriculture Mystical Fertilizer to Garden Cloche Fertilizers.

---
+++ kubejs/server_scripts/normal mode/recipes/Immersive Engineering/Fertilizer/mystical_agriculture.js
@@ -0,0 +1,9 @@
+ServerEvents.recipes(event => {
+    event.custom({
+        type: "immersiveengineering:fertilizer",
+        growthModifier: 2.5,
+        input: {
+            item: 'mysticalagriculture:mystical_fertilizer'
+        }
+    })
+})

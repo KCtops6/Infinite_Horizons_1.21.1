532be7e68e1adb5820aecaf3c49a2b90c1fad359
KCtops6 <kctops06@gmail.com>
2025-05-15 21:42:23 -0400

    Changed Fire Agglomeratio Recipe to make easier early game lava.

---
+++ kubejs/server_scripts/normal mode/recipes/replace_input.js
@@ -0,0 +1,7 @@
+ServerEvents.recipes(event => {
+    event.replaceInput(
+        { output: 'mysticalagriculture:fire_agglomeratio' },
+        'minecraft:lava_bucket',
+        'minecraft:flint'
+    )
+})

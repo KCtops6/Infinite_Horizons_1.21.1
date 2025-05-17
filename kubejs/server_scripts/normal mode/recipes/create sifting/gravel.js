1f44b314080494182e0136fb4b5f5b67d3659482
KCtops6 <kctops06@gmail.com>
2025-05-16 22:46:22 -0400

    Fixed Storage Block Textures.

--- kubejs/server_scripts/normal mode/recipes/create sifting/gravel.js
+++ kubejs/server_scripts/normal mode/recipes/create sifting/gravel.js
@@ -9,6 +9,6 @@ ServerEvents.recipes(event => {
         Output.of('kubejs:raw_silver', 0.1),
         Output.of('kubejs:raw_nickel', 0.15),
         Output.of('kubejs:raw_osmium', 0.1),
-        Output.of('kubejs:raw_uranium', 0.05),
+        Output.of('kubejs:raw_uranium', 0.05)
     ],'minecraft:gravel','createsifter:andesite_mesh')
 })

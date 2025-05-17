700f8083a3f76bf7a49c8afe8778605a2d96447a
KCtops6 <kctops06@gmail.com>
2025-05-16 22:00:10 -0400

    Added KubeJS Raw Tin, Lead, Silver, Nickel, Osmium, Uranium to Andesite Mesh & Gravel Create Sifting Recipe.

--- kubejs/server_scripts/normal mode/recipes/create sifting/gravel.js
+++ kubejs/server_scripts/normal mode/recipes/create sifting/gravel.js
@@ -4,5 +4,11 @@ ServerEvents.recipes(event => {
         Output.of('minecraft:raw_copper', 0.3),
         Output.of('minecraft:raw_gold', 0.15),
         Output.of('create:raw_zinc', 0.2),
+        Output.of('kubejs:raw_tin', 0.25),
+        Output.of('kubejs:raw_lead', 0.1),
+        Output.of('kubejs:raw_silver', 0.1),
+        Output.of('kubejs:raw_nickel', 0.15),
+        Output.of('kubejs:raw_osmium', 0.1),
+        Output.of('kubejs:raw_uranium', 0.05),
     ],'minecraft:gravel','createsifter:andesite_mesh')
 })

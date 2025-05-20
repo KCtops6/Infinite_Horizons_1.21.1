64359dcdf332b3f7363493614fb91fecb0129415
KCtops6 <kctops06@gmail.com>
2025-05-20 11:51:29 -0400

    Changed Waterlogged Dirt Create Sifting Recipe to require Sand.

--- kubejs/server_scripts/normal mode/recipes/create sifting/waterlogged_dirt.js
+++ kubejs/server_scripts/normal mode/recipes/create sifting/waterlogged_dirt.js
@@ -1,9 +1,7 @@
 ServerEvents.recipes(event => {
     event.recipes.createsifter.sifting([
-        Output.of('kubejs:stone_pebble', 1),
-        Output.of('kubejs:andesite_pebble', 0.75),
         Output.of('minecraft:kelp', 0.2),
         Output.of('minecraft:seagrass', 0.4),
         Output.of('minecraft:nautilus_shell', 0.01),
-    ],'minecraft:dirt','createsifter:andesite_mesh').waterlogged(true)
+    ],'minecraft:sand','createsifter:andesite_mesh').waterlogged(true)
 })

1e5a67e6757f48632b8801717bdc16d4a1817714
KCtops6 <kctops06@gmail.com>
2025-05-20 11:52:18 -0400

    Added Sand to the above recipe.

--- kubejs/server_scripts/normal mode/recipes/create sifting/waterlogged_dirt.js
+++ kubejs/server_scripts/normal mode/recipes/create sifting/waterlogged_dirt.js
@@ -2,6 +2,7 @@ ServerEvents.recipes(event => {
     event.recipes.createsifter.sifting([
         Output.of('minecraft:kelp', 0.2),
         Output.of('minecraft:seagrass', 0.4),
+        Output.of('minecraft:sugar_cane', 0.15),
         Output.of('minecraft:nautilus_shell', 0.01),
     ],'minecraft:sand','createsifter:andesite_mesh').waterlogged(true)
 })

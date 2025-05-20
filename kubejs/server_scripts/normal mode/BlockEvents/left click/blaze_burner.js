c69bde6891f2bd1d1bc8a487a8ad572cbde5a077
KCtops6 <kctops06@gmail.com>
2025-05-20 13:25:32 -0400

    Fixed bug in which left clicking multiple empty blaze burners would only return one blaze burner.

--- kubejs/server_scripts/normal mode/BlockEvents/left click/blaze_burner.js
+++ kubejs/server_scripts/normal mode/BlockEvents/left click/blaze_burner.js
@@ -1,7 +1,9 @@
+//ignored: false
 BlockEvents.leftClicked(event => {
     const { block, item, player } = event;
     if (block.id === 'minecraft:nether_portal' && item.id === 'create:empty_blaze_burner') {
-        player.mainHandItem = Item.of('create:blaze_burner');
-        player.tell(Text.gold('The burner roars to life!'));
+        let count = item.count
+        player.mainHandItem = Item.of('create:blaze_burner', count)
+        player.tell(Text.gold('The burner roars to life!'))
     }
 });

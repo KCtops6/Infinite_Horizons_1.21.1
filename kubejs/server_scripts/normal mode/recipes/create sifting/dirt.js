9f86d35331dde9cfc8b871185ce48f206b80c9e7
KCtops6 <kctops06@gmail.com>
2025-05-20 12:02:08 -0400

    Added pumpkin seeds and melon seeds to create sifter (dirt).

--- kubejs/server_scripts/normal mode/recipes/create sifting/dirt.js
+++ kubejs/server_scripts/normal mode/recipes/create sifting/dirt.js
@@ -2,6 +2,8 @@ ServerEvents.recipes(event => {
     event.recipes.createsifter.sifting([
         Output.of('kubejs:stone_pebble', 1),
         Output.of('kubejs:andesite_pebble', 0.75),
+        Output.of('minecraft:pumpkin_seeds', 0.2),
+        Output.of('minecraft:melon_seeds', 0.2),
         Output.of('farmersdelight:wild_rice', 0.1)
     ],'minecraft:grass_block','createsifter:andesite_mesh')
 })

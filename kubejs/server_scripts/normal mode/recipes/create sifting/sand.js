124a6482ed54d2574a7317483fc80f583d77594a
KCtops6 <kctops06@gmail.com>
2025-05-20 11:56:36 -0400

    Created Create Sifter Sand Recipe (bone meal and cactus for now).

---
+++ kubejs/server_scripts/normal mode/recipes/create sifting/sand.js
@@ -0,0 +1,7 @@
+//ignored: false
+ServerEvents.recipes(event => {
+    event.recipes.createsifter.sifting([
+        Output.of('minecraft:bone_meal', 0.35),
+        Output.of('minecraft:cactus', 0.15)
+    ],'minecraft:sand','createsifter:andesite_mesh')
+})

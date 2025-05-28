7cd27b84f5fdda2052b133acb71e8cbf88acfa82
KCtops6 <kctops06@gmail.com>
2025-05-28 15:26:58 -0400

    Added deepslate gravel sifting recipe for amethyst shard.

--- kubejs/server_scripts/normal mode/recipes/Create/Sifting/deepslate_gravel.js
+++ kubejs/server_scripts/normal mode/recipes/Create/Sifting/deepslate_gravel.js
@@ -1,6 +1,7 @@
 ServerEvents.recipes(event => {
     event.recipes.createsifter.sifting([
         Output.of('minecraft:raw_gold', 0.2),
+        Output.of('minecraft:amethyst_shard', 0.1),
         Output.of('mysticalagriculture:prosperity_shard', 0.15),
     ],'kubejs:deepslate_gravel','createsifter:andesite_mesh')
 })

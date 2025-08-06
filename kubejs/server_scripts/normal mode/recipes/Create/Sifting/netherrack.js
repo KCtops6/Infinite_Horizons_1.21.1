eb2ac6b40c538f96abf2b01d50f4482be5837f04
KCtops6 <kctops06@gmail.com>
2025-08-06 14:37:40 -0400

    Expanded crushed netherrack sifting to use multiple mesh tiers with increasing chances for fungi, roots, raw gold, and netherite scrap.

--- kubejs/server_scripts/normal mode/recipes/Create/Sifting/netherrack.js
+++ kubejs/server_scripts/normal mode/recipes/Create/Sifting/netherrack.js
@@ -1,6 +1,33 @@
 ServerEvents.recipes(event => {
     event.recipes.createsifter.sifting([
-        Output.of('minecraft:raw_gold', 0.25),
-        Output.of('minecraft:netherite_scrap', 0.025),
-    ],'createsifter:crushed_netherrack','createsifter:andesite_mesh')
+        Output.of('minecraft:crimson_roots', 0.1),
+        Output.of('minecraft:warped_roots', 0.1),
+        Output.of('minecraft:nether_sprouts', 0.1)
+    ],'createsifter:crushed_netherrack',"createsifter:string_mesh")
+    event.recipes.createsifter.sifting([
+        Output.of('minecraft:crimson_roots', 0.2),
+        Output.of('minecraft:warped_roots', 0.2),
+        Output.of('minecraft:nether_sprouts', 0.2),
+        Output.of('minecraft:warped_fungus', 0.1),
+        Output.of('minecraft:crimson_fungus', 0.1)
+    ],'createsifter:crushed_netherrack',"kubejs:copper_mesh")
+    event.recipes.createsifter.sifting([
+        Output.of('minecraft:crimson_roots', 0.1),
+        Output.of('minecraft:warped_roots', 0.1),
+        Output.of('minecraft:nether_sprouts', 0.1),
+        Output.of('minecraft:warped_fungus', 0.2),
+        Output.of('minecraft:crimson_fungus', 0.2),
+        Output.of('minecraft:raw_gold', 0.15)
+    ],'createsifter:crushed_netherrack',"kubejs:iron_mesh")
+    event.recipes.createsifter.sifting([
+        Output.of('minecraft:warped_fungus', 0.1),
+        Output.of('minecraft:crimson_fungus', 0.1),
+        Output.of('minecraft:raw_gold', 0.3),
+        Output.of('minecraft:netherite_scrap', 0.05)
+    ],'createsifter:crushed_netherrack',"createsifter:brass_mesh")
+    event.recipes.createsifter.sifting([
+        Output.of('minecraft:raw_gold', 0.6),
+        Output.of('minecraft:netherite_scrap', 0.15)
+    ],'createsifter:crushed_netherrack',"kubejs:netherite_mesh")
 })
+

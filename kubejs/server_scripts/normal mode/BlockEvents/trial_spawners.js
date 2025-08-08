c155ad96b0d234027c3502ef4f65941ef9ccbca5
KCtops6 <kctops06@gmail.com>
2025-08-07 20:44:59 -0400

    Changed trial spawner mechanic to use more advanced command and to only work at night.

--- kubejs/server_scripts/normal mode/BlockEvents/trial_spawners.js
+++ kubejs/server_scripts/normal mode/BlockEvents/trial_spawners.js
@@ -1,10 +1,19 @@
-BlockEvents.rightClicked('minecraft:copper_grate', event => {
+BlockEvents.rightClicked('minecraft:copper_grate', event => { 
     const { item, level, player, server } = event;
     const { x, y, z } = event.block;
+
+    const timeOfDay = level.getDayTime() % 24000;
+    const isNight = (timeOfDay >= 13000 && timeOfDay <= 23000);
+    if (!isNight) {
+        player.tell(Text.red('This can only be activated at night.'));
+        return;
+    }
+
     if (item.id !== 'minecraft:trial_key') {
         player.tell(Text.red('You need to hold a Trial Key.'));
         return;
     }
+
     const getId = (dx, dy, dz) => level.getBlock(x + dx, y + dy, z + dz).id;
     const cornerTypes = {
         'minecraft:chiseled_tuff': 'breeze',
@@ -22,24 +31,6 @@ BlockEvents.rightClicked('minecraft:copper_grate', event => {
         'minecraft:purpur_block': 'shulker',
         'minecraft:dark_oak_log': 'pillager',
         'minecraft:emerald_block': 'vindicator'
-    };
-
-    const spawnEggs = {
-        'breeze': 'minecraft:breeze_spawn_egg',
-        'blaze': 'minecraft:blaze_spawn_egg',
-        'zombie': 'minecraft:zombie_spawn_egg',
-        'slime': 'minecraft:slime_spawn_egg',
-        'husk': 'minecraft:husk_spawn_egg',
-        'silverfish': 'minecraft:silverfish_spawn_egg',
-        'stray': 'minecraft:stray_spawn_egg',
-        'skeleton': 'minecraft:skeleton_spawn_egg',
-        'piglin': 'minecraft:piglin_spawn_egg',
-        'hoglin': 'minecraft:hoglin_spawn_egg',
-        'ghast': 'minecraft:ghast_spawn_egg',
-        'shulker': 'minecraft:shulker_spawn_egg',
-        'piglin_brute': 'minecraft:piglin_brute_spawn_egg',
-        'pillager': 'minecraft:pillager_spawn_egg',
-        'vindicator': 'minecraft:vindicator_spawn_egg'
     };
 
     const sideOffsets = [
@@ -74,19 +65,11 @@ BlockEvents.rightClicked('minecraft:copper_grate', event => {
         player.tell(Text.red('Invalid corner block for trial.'));
         return;
     }
-    const spawnEggId = spawnEggs[trialName];
-    if (!spawnEggId) {
-        player.tell(Text.red('No spawn egg defined for this trial.'));
-        return;
-    }
-    event.block.set('minecraft:trial_spawner');
+
+    // Run the /setblock command
+    const command = `setblock ${x} ${y} ${z} trial_spawner[ominous=false,trial_spawner_state=active]{normal_config:{spawn_potentials:[{data:{entity:{id:"minecraft:${trialName}"}},weight:1}]},target_cooldown_length:6000} replace`;
+    server.runCommandSilent(command);
+
     player.mainHandItem.count--;
-    player.give(Item.of(spawnEggId));
-    player.tell(Text.green(`You received a ${trialName} spawn egg! Right-click a Trial Chamber to activate.`));
+    player.tell(Text.green(`Trial spawner set for ${trialName}!`));
 });
-
-BlockEvents.rightClicked('minecraft:vault', event => {
-    event.block.set('minecraft:air')
-    event.block.set('minecraft:vault')
-})
-

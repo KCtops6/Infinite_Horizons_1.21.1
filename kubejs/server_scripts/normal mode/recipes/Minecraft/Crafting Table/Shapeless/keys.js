a4f99cd6e33ef89ab841f838f5b1a743588b4f4c
KCtops6 <kctops06@gmail.com>
2025-08-08 09:53:04 -0400

    Revert "Added mechanic to create trial vault."

---
+++ kubejs/server_scripts/normal mode/recipes/Minecraft/Crafting Table/Shapeless/keys.js
@@ -0,0 +1,8 @@
+ServerEvents.recipes(event => {
+    event.shapeless('kubejs:end_key', [
+        'kubejs:bastion_key',
+        'kubejs:ruined_portal_key',
+        'kubejs:fortress_key',
+        'minecraft:ender_eye'
+    ])
+})

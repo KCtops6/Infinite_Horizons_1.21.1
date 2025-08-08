a6d464d7e8505967532862b865f105b357e607d2
KCtops6 <kctops06@gmail.com>
2025-08-08 10:42:17 -0400

    Removed basic end key crafting recipe.

--- kubejs/server_scripts/normal mode/recipes/Minecraft/Crafting Table/Shapeless/keys.js
+++
@@ -1,8 +0,0 @@
-ServerEvents.recipes(event => {
-    event.shapeless('kubejs:end_key', [
-        'kubejs:bastion_key',
-        'kubejs:ruined_portal_key',
-        'kubejs:fortress_key',
-        'minecraft:ender_eye'
-    ])
-})

9aecbaad5490c93565f65e0e4b69ca0ccfc34a84
KCtops6 <kctops06@gmail.com>
2025-08-27 17:42:58 -0400

    Added recipes for diorite and granite using their pebbles.

--- kubejs/server_scripts/normal mode/recipes/Minecraft/Crafting Table/Shapeless/two_by_two.js
+++ kubejs/server_scripts/normal mode/recipes/Minecraft/Crafting Table/Shapeless/two_by_two.js
@@ -2,9 +2,11 @@ ServerEvents.recipes(event => {
     const mappings = [
         { input: 'kubejs:stone_pebble', output: 'minecraft:cobblestone'},
         { input: 'kubejs:andesite_pebble', output: 'minecraft:andesite'},
-        { input: 'kubejs:deepslate_pebble', output: 'minecraft:cobbled_deepslate'}
+        { input: 'kubejs:deepslate_pebble', output: 'minecraft:cobbled_deepslate'},
+        { input: 'kubejs:diorite_pebble', output: 'minecraft:diorite'},
+        { input: 'kubejs:granite_pebble', output: 'minecraft:granite'}
     ]
     mappings.forEach(recipe => {
         event.shapeless(recipe.output, Item.of(recipe.input, 4))
     })
-})
+});

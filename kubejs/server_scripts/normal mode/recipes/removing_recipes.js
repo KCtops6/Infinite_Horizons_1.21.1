18e2d4aacb9ba112122f8105b43f461f9e7dbc53
KCtops6 <kctops06@gmail.com>
2025-08-31 09:20:56 -0400

    remove other mods' doughs that are not create.

--- kubejs/server_scripts/normal mode/recipes/removing_recipes.js
+++ kubejs/server_scripts/normal mode/recipes/removing_recipes.js
@@ -4,4 +4,12 @@ ServerEvents.recipes(event => {
     materials.forEach(material => {
         event.remove({ id: `immersiveengineering:metalpress/wire_${material}` })    // remove each matalpress recipe for that wire.
     })
+
+    /**
+     * Remove other mod doughs with Create's dough
+     * Create's dough is used in more recipes and is more versatile
+     * than the other mod doughs.
+     */
+    event.remove({ output: 'farmersdelight:wheat_dough' }); // Remove Farmer's Delight wheat dough
+    event.remove({ output: 'pamhc2foodcore:doughitem' });   // Remove Pam's HarvestCraft dough item
 })

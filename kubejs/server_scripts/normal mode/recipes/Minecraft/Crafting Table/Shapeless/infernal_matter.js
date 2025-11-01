a7a5b83c99a91c25cb83f0e7072cdde66a90611f
KCtops6 <kctops06@gmail.com>
2025-11-01 14:11:06 -0400

    Added recipe for infernal matter item.

---
+++ kubejs/server_scripts/normal mode/recipes/Minecraft/Crafting Table/Shapeless/infernal_matter.js
@@ -0,0 +1,4 @@
+ServerEvents.recipes(event => {
+    event.shapeless(Item.of('kubejs:infernal_matter', 1), [
+        'create_deep_dark:sculk_flour', 'create:cinder_flour', 'minecraft:blaze_powder']);
+});

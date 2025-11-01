56b2c0b49c6a5cdf358e2ecb893beb3216dfd08a
KCtops6 <kctops06@gmail.com>
2025-11-01 15:59:52 -0400

    Revamped base productive bee spawns. No need for nests anymore.

--- kubejs/server_scripts/normal mode/BlockEvents/bee_spawning.js
+++ kubejs/server_scripts/normal mode/BlockEvents/bee_spawning.js
@@ -1,20 +1,79 @@
-BlockEvents.rightClicked('minecraft:bee_nest', event => {
-    const { block, item, player, level } = event;
-    const { x, y, z } = block;
-
+BlockEvents.rightClicked(event => {
+    const { block, item, player } = event;
     if (item.id !== 'productivebees:honey_treat') return;
-
-    const belowBlock = level.getBlock(x, y - 1, z).id;
-
-    // Map blocks to the bee eggs
     const beeMap = {
-        'minecraft:glowstone': 'productivebees:spawn_egg_configurable_bee[entity_data={id:"productivebees:configurable_bee",type:"productivebees:glowing"}]',
-        'minecraft:coarse_dirt': 'productivebees:spawn_egg_chocolate_mining_bee'
+        'minecraft:oak_log': [
+            'productivebees:spawn_egg_green_carpenter_bee',
+            'productivebees:spawn_egg_yellow_black_carpenter_bee'
+        ],
+        'minecraft:spruce_log': [
+            'productivebees:spawn_egg_resin_bee',
+            'productivebees:spawn_egg_yellow_black_carpenter_bee'
+        ],
+        'minecraft:dark_oak_log': [
+            'productivebees:spawn_egg_green_carpenter_bee',
+            'productivebees:spawn_egg_yellow_black_carpenter_bee',
+            'productivebees:spawn_egg_blue_banded_bee'
+        ],
+        'minecraft:birch_log': [
+            'productivebees:spawn_egg_green_carpenter_bee',
+            'productivebees:spawn_egg_yellow_black_carpenter_bee'
+        ],
+        'minecraft:jungle_log': ['productivebees:spawn_egg_green_carpenter_bee'],
+        'minecraft:acacia_log': ['productivebees:spawn_egg_blue_banded_bee'],
+        'minecraft:cherry_log': ['productivebees:spawn_egg_blue_banded_bee'],
+        'minecraft:mangrove_log': ['productivebees:spawn_egg_green_carpenter_bee'],
+        'minecraft:stone': [
+            'productivebees:spawn_egg_mason_bee',
+            'productivebees:spawn_egg_digger_bee'
+        ],
+        'minecraft:coarse_dirt': [
+            'productivebees:spawn_egg_leafcutter_bee',
+            'productivebees:spawn_egg_chocolate_mining_bee',
+            'productivebees:spawn_egg_ashy_mining_bee',
+        ],
+        'minecraft:sand': [
+            'productivebees:spawn_egg_chocolate_mining_bee',
+            'productivebees:spawn_egg_ashy_mining_bee',
+        ],
+        'minecraft:snow_block': ['productivebees:spawn_egg_sweat_bee'],
+        'minecraft:gravel': [
+            'productivebees:spawn_egg_digger_bee',
+            'productivebees:spawn_egg_chocolate_mining_bee',
+            'productivebees:spawn_egg_ashy_mining_bee',
+        ],
+        'minecraft:sugar_cane': [
+            'productivebees:spawn_egg_mason_bee',
+            'productivebees:spawn_egg_reed_bee'
+        ],
+        'minecraft:slime_block': [
+            'productivebees:spawn_egg_configurable_bee[entity_data={id:"productivebees:configurable_bee",type:"productivebees:slimy"}]'
+        ],
+        'minecraft:glowstone': [
+            'productivebees:spawn_egg_configurable_bee[entity_data={id:"productivebees:configurable_bee",type:"productivebees:glowing"}]'
+        ],
+        'minecraft:soul_sand': [
+            'productivebees:spawn_egg_configurable_bee[entity_data={id:"productivebees:configurable_bee",type:"productivebees:ghostly"}]'
+        ],
+        'minecraft:quartz_block': [
+            'productivebees:spawn_egg_configurable_bee[entity_data={id:"productivebees:configurable_bee",type:"productivebees:crystalline"}]'
+        ],
+        'minecraft:gold_block': [
+            'productivebees:spawn_egg_configurable_bee[entity_data={id:"productivebees:configurable_bee",type:"productivebees:gold"}]'
+        ],
+        'minecraft:nether_bricks': [
+            'productivebees:spawn_egg_configurable_bee[entity_data={id:"productivebees:configurable_bee",type:"productivebees:magmatic"}]'
+        ],
+        'minecraft:end_stone': [
+            'productivebees:spawn_egg_configurable_bee[entity_data={id:"productivebees:configurable_bee",type:"productivebees:end"}]'
+        ],
+        'minecraft:obsidian': [
+            'productivebees:spawn_egg_configurable_bee[entity_data={id:"productivebees:configurable_bee",type:"productivebees:draconic"}]'
+        ],
     };
-
-    const beeEgg = beeMap[belowBlock];
-    if (beeEgg) {
-        player.runCommandSilent(`give @s ${beeEgg}`);
-        player.mainHandItem.count--;
-    }
+    if (!(block.id in beeMap)) return;
+    const bees = beeMap[block.id];
+    const randomBee = bees[Math.floor(Math.random() * bees.length)];
+    player.runCommandSilent(`give @s ${randomBee}`);
+    player.mainHandItem.count--;
 });

40f56aef78a3c17914853edb482e1f95dd032454
KCtops6 <kctops06@gmail.com>
2025-11-01 16:00:37 -0400

    Removed all productive bee nests from JEI.

--- kubejs/server_scripts/normal mode/RecipeViewerEvents/removeEntries.js
+++ kubejs/server_scripts/normal mode/RecipeViewerEvents/removeEntries.js
@@ -1,3 +1,7 @@
 RecipeViewerEvents.removeEntries('item', event => {
 	event.remove('kubejs:incomplete_nether_totem')
+	global.nests.forEach(n => {
+		event.remove(`productivebees:${n}_nest`)
+	})
+	event.remove('productivebees:bamboo_hive')
 })

d12ad715c62e4642adb584c82c880d0305704d1b
KCtops6 <kctops06@gmail.com>
2025-11-28 18:56:17 -0500

    removed unused block from tree sap script.

--- kubejs/server_scripts/normal mode/LootJS/modifiers/block/tree_sap.js
+++ kubejs/server_scripts/normal mode/LootJS/modifiers/block/tree_sap.js
@@ -6,7 +6,5 @@ LootJS.modifiers(event => {
             .matchTool("#minecraft:axes")   // ...with an axe...
             .randomChance(0.25)
             .addLoot(LootEntry.of("kubejs:tree_sap"))
-            .addLoot(LootEntry.of(`kubejs:scared_${tree}_log`))
-            .removeLoot(`minecraft:${tree}_log`);   // ...you have a 25% chance of getting tree sap as an addition.
     });
 })

5f0126de8c1d134d496eeae136ddc277899ef2eb
KCtops6 <kctops06@gmail.com>
2025-11-30 13:44:45 -0500

    Modified gravel sifting results.

--- kubejs/server_scripts/normal mode/recipes/Create/Sifting/gravel.js
+++ kubejs/server_scripts/normal mode/recipes/Create/Sifting/gravel.js
@@ -1,43 +1,29 @@
-//ignored: false
-function ironMultiplier(x) { return x * 1.2 }   // 20% increase over copper
-function brassMultiplier(x) { return ironMultiplier(x) * 1.4 }  // 40% increase over iron
-function netheriteMultiplier(x) { return brassMultiplier(x) * 1.6 } // 60% increase over brass
 ServerEvents.recipes(event => {
     event.recipes.createsifter.sifting([
-        Output.of('minecraft:coal', 0.3),
+        Output.of('minecraft:coal', 0.25),
         Output.of('minecraft:raw_iron', 0.25),
-        Output.of('minecraft:raw_copper', 0.3),
-        Output.of('minecraft:raw_gold', 0.15),
-        Output.of('create:raw_zinc', 0.2),
-        Output.of('kubejs:raw_tin', 0.25),
-        Output.of('kubejs:raw_lead', 0.1)
+        Output.of('minecraft:raw_copper', 0.5),
+        Output.of('create:raw_zinc', 0.125)
     ],'minecraft:gravel','kubejs:copper_mesh')
     event.recipes.createsifter.sifting([
-        Output.of('minecraft:coal', ironMultiplier(0.3)),
-        Output.of('minecraft:raw_iron', ironMultiplier(0.25)),
-        Output.of('minecraft:raw_copper', ironMultiplier(0.3)),
-        Output.of('minecraft:raw_gold', ironMultiplier(0.15)),
-        Output.of('create:raw_zinc', ironMultiplier(0.2)),
-        Output.of('kubejs:raw_tin', ironMultiplier(0.25)),
-        Output.of('kubejs:raw_lead', ironMultiplier(0.1))
+        Output.of('minecraft:coal', 0.5),
+        Output.of('minecraft:raw_iron', 0.5),
+        Output.of('minecraft:raw_copper', 0.75),
+        Output.of('create:raw_zinc', 0.25),
+        Output.of('kubejs:raw_tin', 0.25)
     ],'minecraft:gravel','kubejs:iron_mesh')
     event.recipes.createsifter.sifting([
-        Output.of('minecraft:coal', brassMultiplier(0.3)),
-        Output.of('minecraft:raw_iron', brassMultiplier(0.25)),
-        Output.of('minecraft:raw_copper', brassMultiplier(0.3)),
-        Output.of('minecraft:raw_gold', brassMultiplier(0.15)),
-        Output.of('create:raw_zinc', brassMultiplier(0.2)),
-        Output.of('kubejs:raw_tin', brassMultiplier(0.25)),
-        Output.of('kubejs:raw_lead', brassMultiplier(0.1))
+        Output.of('minecraft:coal', 0.25),
+        Output.of('minecraft:raw_iron', 0.75),
+        Output.of('minecraft:raw_copper', 0.5),
+        Output.of('create:raw_zinc', 0.5),
+        Output.of('kubejs:raw_tin', 0.5)
     ],'minecraft:gravel','createsifter:brass_mesh')
     event.recipes.createsifter.sifting([
-        Output.of('minecraft:coal', netheriteMultiplier(0.3)),
-        Output.of('minecraft:raw_iron', netheriteMultiplier(0.25)),
-        Output.of('minecraft:raw_copper', netheriteMultiplier(0.3)),
-        Output.of('minecraft:raw_gold', netheriteMultiplier(0.15)),
-        Output.of('create:raw_zinc', netheriteMultiplier(0.2)),
-        Output.of('kubejs:raw_tin', netheriteMultiplier(0.25)),
-        Output.of('kubejs:raw_lead', netheriteMultiplier(0.1))
+        Output.of('minecraft:coal', 0.125),
+        Output.of('minecraft:raw_iron', 0.5),
+        Output.of('minecraft:raw_copper', 0.25),
+        Output.of('create:raw_zinc', 0.75),
+        Output.of('kubejs:raw_tin', 0.75),
     ],'minecraft:gravel','kubejs:netherite_mesh')
-    
 })

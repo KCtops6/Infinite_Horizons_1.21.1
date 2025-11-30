b07eb95b420cb04d02f8edd83c57511f27a03b38
KCtops6 <kctops06@gmail.com>
2025-11-30 14:04:16 -0500

    Modified sand sifting results.

--- kubejs/server_scripts/normal mode/recipes/Create/Sifting/sand.js
+++ kubejs/server_scripts/normal mode/recipes/Create/Sifting/sand.js
@@ -3,5 +3,5 @@ ServerEvents.recipes(event => {
     event.recipes.createsifter.sifting([
         Output.of('minecraft:bone_meal', 0.35),
         Output.of('minecraft:cactus', 0.15)
-    ],'minecraft:sand','createsifter:andesite_mesh')
+    ],'minecraft:sand','createsifter:string_mesh')
 })

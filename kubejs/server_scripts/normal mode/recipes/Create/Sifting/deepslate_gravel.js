1ab9a93c0373cf01d14aadb8bf7a8b4606be06d4
KCtops6 <kctops06@gmail.com>
2025-11-30 13:59:49 -0500

    Modified deepslate gravel sifting results.

--- kubejs/server_scripts/normal mode/recipes/Create/Sifting/deepslate_gravel.js
+++ kubejs/server_scripts/normal mode/recipes/Create/Sifting/deepslate_gravel.js
@@ -1,43 +1,43 @@
 ServerEvents.recipes(event => {
     event.recipes.createsifter.sifting([
-        Output.of('kubejs:raw_aluminum', 0.025),
-        Output.of('kubejs:raw_lead', 0.05),
-        Output.of('kubejs:raw_nickel', 0.05),
-        Output.of('kubejs:raw_osmium', 0.1),
-        Output.of('kubejs:raw_silver', 0.05),
-        Output.of('kubejs:raw_uranium', 0.0125)
-    ],'kubejs:deepslate_gravel','kubejs:copper_mesh')
+        Output.of('kubejs:raw_aluminum', 0.125),
+        Output.of('kubejs:raw_lead', 0.125),
+        Output.of('kubejs:raw_nickel', 0.125),
+        Output.of('kubejs:raw_silver', 0.125),
+        Output.of('kubejs:raw_osmium', 0.25),
+        Output.of('kubejs:raw_uranium', 0.0625)
+    ], 'kubejs:deepslate_gravel', 'kubejs:copper_mesh')
     event.recipes.createsifter.sifting([
-        Output.of('minecraft:diamond', 0.1),
-        Output.of('kubejs:raw_aluminum', 0.025 * 1.5),
-        Output.of('kubejs:raw_lead', 0.05 * 1.5),
-        Output.of('kubejs:raw_nickel', 0.05 * 1.5),
-        Output.of('kubejs:raw_osmium', 0.1 * 1.5),
-        Output.of('kubejs:raw_silver', 0.05 * 1.5),
-        Output.of('kubejs:raw_uranium', 0.0125 * 1.5)
-    ],'kubejs:deepslate_gravel','kubejs:iron_mesh')
+        Output.of('minecraft:diamond', 0.125),
+        Output.of('kubejs:raw_aluminum', 0.25),
+        Output.of('kubejs:raw_lead', 0.25),
+        Output.of('kubejs:raw_nickel', 0.25),
+        Output.of('kubejs:raw_silver', 0.25),
+        Output.of('kubejs:raw_osmium', 0.5),
+        Output.of('kubejs:raw_uranium', 0.125)
+    ], 'kubejs:deepslate_gravel', 'kubejs:iron_mesh')
     event.recipes.createsifter.sifting([
-        Output.of('minecraft:diamond', 0.1 * 1.5),
-        Output.of('minecraft:raw_gold', 0.2),
-        Output.of('kubejs:raw_aluminum', 0.025 * 1.5 * 2),
-        Output.of('kubejs:raw_lead', 0.05 * 1.5 * 2),
-        Output.of('kubejs:raw_nickel', 0.05 * 1.5 * 2),
-        Output.of('kubejs:raw_osmium', 0.1 * 1.5 * 2),
-        Output.of('kubejs:raw_silver', 0.05 * 1.5 * 2),
-        Output.of('kubejs:raw_uranium', 0.0125 * 1.5 * 2),
-        Output.of('minecraft:amethyst_shard', 0.1 * 2),
-        Output.of('mysticalagriculture:prosperity_shard', 0.15)
-    ],'kubejs:deepslate_gravel','createsifter:brass_mesh')
-    event.recipes.createsifter.sifting([
-        Output.of('minecraft:diamond', 0.1 * 1.5 * 2),
-        Output.of('minecraft:raw_gold', 0.3),
-        Output.of('kubejs:raw_aluminum', 0.025 * 1.5 * 2 * 2.5),
-        Output.of('kubejs:raw_lead', 0.05 * 1.5 * 2 * 2.5),
-        Output.of('kubejs:raw_nickel', 0.05 * 1.5 * 2 * 2.5),
-        Output.of('kubejs:raw_osmium', 0.1 * 1.5 * 2 * 2.5),
-        Output.of('kubejs:raw_silver', 0.05 * 1.5 * 2 * 2.5),
-        Output.of('kubejs:raw_uranium', 0.0125 * 1.5 * 2 * 2.5),
-        Output.of('minecraft:amethyst_shard', 0.2),
+        Output.of('minecraft:diamond', 0.25),
+        Output.of('minecraft:raw_gold', 0.5),
+        Output.of('kubejs:raw_aluminum', 0.125),
+        Output.of('kubejs:raw_lead', 0.125),
+        Output.of('kubejs:raw_nickel', 0.125),
+        Output.of('kubejs:raw_silver', 0.125),
+        Output.of('kubejs:raw_osmium', 0.75),
+        Output.of('minecraft:amethyst_shard', 0.5),
+        Output.of('kubejs:raw_uranium', 0.25),
         Output.of('mysticalagriculture:prosperity_shard', 0.25)
-    ],'kubejs:deepslate_gravel','kubejs:netherite_mesh')
+    ], 'kubejs:deepslate_gravel', 'createsifter:brass_mesh')
+    event.recipes.createsifter.sifting([
+        Output.of('minecraft:diamond', 0.75),
+        Output.of('minecraft:raw_gold', 0.25),
+        Output.of('kubejs:raw_aluminum', 0.0625),
+        Output.of('kubejs:raw_lead', 0.0625),
+        Output.of('kubejs:raw_nickel', 0.0625),
+        Output.of('kubejs:raw_silver', 0.0625),
+        Output.of('kubejs:raw_osmium', 0.5),
+        Output.of('minecraft:amethyst_shard', 0.25),
+        Output.of('mysticalagriculture:prosperity_shard', 0.75),
+        Output.of('kubejs:raw_uranium', 0.75)
+    ], 'kubejs:deepslate_gravel', 'kubejs:netherite_mesh')
 })

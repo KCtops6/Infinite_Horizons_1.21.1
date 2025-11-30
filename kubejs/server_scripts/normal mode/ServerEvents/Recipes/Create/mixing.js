2e69569541975e0d11d0df0073b41fb904eeb786
KCtops6 <kctops06@gmail.com>
2025-11-30 15:17:26 -0500

    merged all create mixing scripts into one

--- kubejs/server_scripts/normal mode/ServerEvents/Recipes/Create/mixing.js
+++ kubejs/server_scripts/normal mode/ServerEvents/Recipes/Create/mixing.js
@@ -1,6 +1,19 @@
-//ignored: false
-//requires: immersiveengineering
 ServerEvents.recipes(event => {
+    event.custom({
+        type: "create:mixing",
+        heatRequirement: "heated",
+        ingredients: [
+            {
+                tag: "infinitehorizons:pebbles"
+            }
+        ],
+        results: [
+            {
+                amount: 250,
+                id: "minecraft:lava"
+            }
+        ]
+    })
     event.custom({
         type: "create:mixing",
         ingredients: [

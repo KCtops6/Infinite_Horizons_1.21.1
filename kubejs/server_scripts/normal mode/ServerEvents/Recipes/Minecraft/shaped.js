8c093b82b3f0484ee4ff2f2e34d91e9ffee55efd
KCtops6 <kctops06@gmail.com>
2025-12-20 18:05:29 -0500

    added block from ingot and ingot from nugget recipes for kubejs materials.

---
+++ kubejs/server_scripts/normal mode/ServerEvents/Recipes/Minecraft/shaped.js
@@ -0,0 +1,33 @@
+ServerEvents.recipes(event => {
+    global.ingots.forEach(i => {
+        if (global.nuggets.includes(i)) {
+            event.shaped(`kubejs:${i}_ingot`, [
+                'NNN',
+                'NNN',
+                'NNN'
+            ], {
+                N: `kubejs:${i}_nugget`
+            }).id(`${i}_ingot_from_nugget`);
+        }
+        if (global.blocks.includes(i)) {
+            event.shaped(`kubejs:${i}_block`, [
+                'III',
+                'III',
+                'III'
+            ], {
+                I: `kubejs:${i}_ingot`
+            }).id(`${i}_block_from_ingot`);
+        }
+    });
+    global.gems.forEach(i => {
+        if (global.blocks.includes(i)) {
+            event.shaped(`kubejs:${i}_block`, [
+                'III',
+                'III',
+                'III'
+            ], {
+                I: `kubejs:${i}_gem`
+            }).id(`${i}_block_from_gem`);
+        }
+    });
+});

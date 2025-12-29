893f7c75074ddc63a648c354b36f7ec18502b51c
KCtops6 <kctops06@gmail.com>
2025-12-29 15:14:14 -0500

    Added milling recipes for all KubeJS material dusts.

--- kubejs/server_scripts/normal mode/ServerEvents/Recipes/Create/milling.js
+++ kubejs/server_scripts/normal mode/ServerEvents/Recipes/Create/milling.js
@@ -18,5 +18,38 @@ ServerEvents.recipes(event => {
                 }
             ]
         }).id(`kubejs:milling/${s}_to_${s}_gravel`);
+    });
+    global.dusts.forEach(dust => {
+        if (global.ingots.includes(dust)) {
+            event.custom({
+                type: "create:milling",
+                ingredients: [
+                    {
+                        tag: `c:ingots/${dust}`
+                    }
+                ],
+                processing_time: 250,
+                results: [
+                    {
+                        id: `kubejs:${dust}_dust`
+                    }
+                ]
+            }).id(`kubejs:milling/${dust}_ingot_to_${dust}_dust`);
+        } else if (global.gems.includes(dust)) {
+            event.custom({
+                type: "create:milling",
+                ingredients: [
+                    {
+                        tag: `c:gems/${dust}`
+                    }
+                ],
+                processing_time: 250,
+                results: [
+                    {
+                        id: `kubejs:${dust}_dust`
+                    }
+                ]
+            }).id(`kubejs:milling/${dust}_gem_to_${dust}_dust`);
+        }
     })
-})
+});

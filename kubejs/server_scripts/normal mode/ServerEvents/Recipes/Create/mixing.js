365b723d24edc9c3c6ba71f24992bd70c9c039c4
KCtops6 <kctops06@gmail.com>
2026-01-09 08:17:25 -0500

    Added mixing recipes for quartz enriched iron/copper

--- kubejs/server_scripts/normal mode/ServerEvents/Recipes/Create/mixing.js
+++ kubejs/server_scripts/normal mode/ServerEvents/Recipes/Create/mixing.js
@@ -13,7 +13,7 @@ ServerEvents.recipes(event => {
                 id: "minecraft:lava"
             }
         ]
-    })
+    });
     event.custom({
         type: "create:mixing",
         ingredients: [
@@ -32,5 +32,83 @@ ServerEvents.recipes(event => {
                 id: "immersiveengineering:redstone_acid"
             }
         ]
-    })
-})
+    });
+    if (Platform.isLoaded('refinedstorage')) {
+        event.custom({
+            type: "create:mixing",
+            ingredients: [
+                {
+                    item: 'kubejs:iron_dust'
+                },
+                {
+                    item: 'kubejs:iron_dust'
+                },
+                {
+                    item: 'kubejs:iron_dust'
+                },
+                {
+                    item: 'kubejs:iron_dust'
+                },
+                {
+                    item: 'kubejs:iron_dust'
+                },
+                {
+                    item: 'kubejs:nether_quartz_dust'
+                },
+                {
+                    item: 'kubejs:nether_quartz_dust'
+                },
+                {
+                    item: 'kubejs:nether_quartz_dust'
+                },
+                {
+                    item: 'kubejs:nether_quartz_dust'
+                }
+            ],
+            results: [
+                {
+                    count: 9,
+                    id: 'kubejs:quartz_enriched_iron_dust'
+                }
+            ]
+        });
+        event.custom({
+            type: "create:mixing",
+            ingredients: [
+                {
+                    item: 'kubejs:copper_dust'
+                },
+                {
+                    item: 'kubejs:copper_dust'
+                },
+                {
+                    item: 'kubejs:copper_dust'
+                },
+                {
+                    item: 'kubejs:copper_dust'
+                },
+                {
+                    item: 'kubejs:copper_dust'
+                },
+                {
+                    item: 'kubejs:nether_quartz_dust'
+                },
+                {
+                    item: 'kubejs:nether_quartz_dust'
+                },
+                {
+                    item: 'kubejs:nether_quartz_dust'
+                },
+                {
+                    item: 'kubejs:nether_quartz_dust'
+                }
+            ],
+            results: [
+                {
+                    count: 9,
+                    id: 'kubejs:quartz_enriched_copper_dust'
+                }
+            ]
+        });
+    }
+});

86b558fc28182c0ae67bddcb4ac7c36ec3b93f99
KCtops6 <kctops06@gmail.com>
2026-01-16 19:14:31 -0500

    Added decompressing recipe for Echo Blocks.

--- kubejs/server_scripts/normal mode/ServerEvents/Recipes/Minecraft/shapeless.js
+++ kubejs/server_scripts/normal mode/ServerEvents/Recipes/Minecraft/shapeless.js
@@ -9,4 +9,5 @@ ServerEvents.recipes(event => {
             event.shapeless(Item.of(`kubejs:${i}_ingot`, 9), [`kubejs:${i}_block`]);
         }
     });
+    event.shapeless(Item.of('create_deep_dark:echo_ingot, 9'), ['create_deep_dark:echo_block']);
 });

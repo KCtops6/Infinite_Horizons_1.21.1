ac3689e1bfc51d3397dd967ffefdda2a2188a063
KCtops6 <kctops06@gmail.com>
2026-02-07 10:53:34 -0500

    Fixed error in which you could not blast most raw ores into ingots.

--- kubejs/server_scripts/normal mode/ServerEvents/Recipes/Minecraft/blasting.js
+++ kubejs/server_scripts/normal mode/ServerEvents/Recipes/Minecraft/blasting.js
@@ -1,3 +1,6 @@
 ServerEvents.recipes(event => {
     event.blasting('create:scorchia', 'create:scoria', 0.25, 175)
+    global.raw_ores.forEach(i => {
+        event.blasting(`kubejs:${i}_ingot`, `kubejs:raw_${i}`).id(`${i}_ingot_from_raw_${i}`);
+    });
 })

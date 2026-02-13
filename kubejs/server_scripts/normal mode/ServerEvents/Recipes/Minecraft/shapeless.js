0613b76146c1cc133ddfb98ab8bc0454acf3d076
KCtops6 <kctops06@gmail.com>
2026-02-13 09:35:45 -0500

    Fixed error in which you can not craft raw ores from raw ore blocks.

--- kubejs/server_scripts/normal mode/ServerEvents/Recipes/Minecraft/shapeless.js
+++ kubejs/server_scripts/normal mode/ServerEvents/Recipes/Minecraft/shapeless.js
@@ -9,5 +9,10 @@ ServerEvents.recipes(event => {
             event.shapeless(Item.of(`kubejs:${i}_ingot`, 9), [`kubejs:${i}_block`]);
         }
     });
+    global.raw_ores.forEach(i => {
+        if (global.raw_ore_blocks.includes(i)) {
+            event.shapeless(Item.of(`kubejs:raw_${i}`, 9), [`kubejs:block_of_raw_${i}`]);
+        }
+    });
     event.shapeless(Item.of('create_deep_dark:echo_ingot', 9), ['create_deep_dark:echo_block']);
 });

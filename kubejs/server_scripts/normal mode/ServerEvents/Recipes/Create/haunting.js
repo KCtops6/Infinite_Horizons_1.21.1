a9c79dc7d30b9a4b2e22155e25a1773adafada60
KCtops6 <kctops06@gmail.com>
2026-03-02 14:21:35 -0500

    Added haunting recipe for end stone.

--- kubejs/server_scripts/normal mode/ServerEvents/Recipes/Create/haunting.js
+++ kubejs/server_scripts/normal mode/ServerEvents/Recipes/Create/haunting.js
@@ -1,15 +1,14 @@
 ServerEvents.recipes(event => {
+    // soulstone
     event.custom({
         type: "create:haunting",
-        ingredients: [
-            {
-                item: "minecraft:blackstone"
-            }
-        ],
-        results: [
-            {
-                id: "mysticalagriculture:soulstone"
-            }
-        ]
-    }).id('create/haunting/soulstone');
+        ingredients: [{ item: "minecraft:blackstone" }],
+        results: [{ id: "mysticalagriculture:soulstone" }]
+    }).id('create:haunting/soulstone');
+    // end stone
+    event.custom({
+        type: "create:haunting",
+        ingredients: [{ item: "minecraft:netherrack" }],
+        results: [{ id: "minecraft:end_stone" }]
+    }).id('create:haunting/end_stone');
 });

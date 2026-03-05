a9c0872d405543abef6e92945e7ad9b3770ced00
KCtops6 <kctops06@gmail.com>
2026-03-04 19:22:43 -0500

    Refactor splashing recipe logic to check item existence before processing

--- kubejs/server_scripts/normal mode/ServerEvents/Recipes/Create/splashing.js
+++ kubejs/server_scripts/normal mode/ServerEvents/Recipes/Create/splashing.js
@@ -1,18 +1,21 @@
 ServerEvents.recipes(event => {
     global.raw_ores.forEach(i => {
-        event.custom({
-            type: "create:splashing",
-            ingredients: [
-                {
-                    item: `create:crushed_raw_${i}`
-                }
-            ],
-            results: [
-                {
-                    count: 9,
-                    id: `kubejs:${i}_nugget`
-                }
-            ]
-        }).id(`kubejs:create_splashing/${i}_nugget`);
+        let itemId = `create:crushed_raw_${i}`;
+        if (Item.exists(itemId)) {
+            event.custom({
+                type: "create:splashing",
+                ingredients: [
+                    {
+                        item: itemId
+                    }
+                ],
+                results: [
+                    {
+                        count: 9,
+                        id: `kubejs:${i}_nugget`
+                    }
+                ]
+            }).id(`kubejs:create_splashing/${i}_nugget`);
+        }
     });
 });

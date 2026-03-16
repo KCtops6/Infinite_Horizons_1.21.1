db2af80f2bda379c9054fa368bf70e74941d6f52
KCtops6 <kctops06@gmail.com>
2026-03-16 10:14:40 -0400

    Added rolling recipes for pam's harvestcraft rolling pin item

---
+++ kubejs/server_scripts/normal mode/ServerEvents/Recipes/Create/rolling.js
@@ -0,0 +1,21 @@
+ServerEvents.recipes(event => {
+    const ROLLER_ITEM = 'pamhc2foodcore:rolleritem';
+    event.forEachRecipe({ input: ROLLER_ITEM }, recipe => {
+        let ingredientsJson = [];
+        recipe.getOriginalRecipeIngredients().forEach(i => {
+            if (!i.test(ROLLER_ITEM)) {
+                ingredientsJson.push(i.toJson());
+            }
+        });
+        let resultJson = [{
+            count: recipe.originalRecipeResult.count * 2,
+            id: recipe.originalRecipeResult.id
+        }];
+        let cleanID = recipe.originalRecipeResult.id.replace(':', '_');
+        event.custom({
+            type: "createaddition:rolling",
+            ingredients: ingredientsJson,
+            results: resultJson
+        }).id(`createaddition:rolling/kubejs/${cleanID}`); 
+    });
+});

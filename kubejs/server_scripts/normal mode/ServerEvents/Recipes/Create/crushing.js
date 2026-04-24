07d4ca05cedeabfc30870cdf15bf88e46f8dbb93
KCtops6 <kctops06@gmail.com>
2026-04-24 15:27:03 -0400

    Fixed create crushing (pam's grinder) oversight

--- kubejs/server_scripts/normal mode/ServerEvents/Recipes/Create/crushing.js
+++ kubejs/server_scripts/normal mode/ServerEvents/Recipes/Create/crushing.js
@@ -11,22 +11,24 @@ ServerEvents.recipes(event => {
 
     const GRINDER_ITEM = 'pamhc2foodcore:grinderitem';
     event.forEachRecipe({ input: GRINDER_ITEM }, recipe => {
-        let ingredientsJson = [];
-        recipe.getOriginalRecipeIngredients().forEach(i => {
-            if (!i.test(GRINDER_ITEM)) {
-                ingredientsJson.push(i.toJson());
-            }
-        });
-        let resultJson = [{
-            count: recipe.originalRecipeResult.count * 3,
-            id: recipe.originalRecipeResult.id
-        }];
-        let cleanID = recipe.originalRecipeResult.id.replace(':', '_');
-        event.custom({
-            type: "create:crushing",
-            ingredients: ingredientsJson,
-            processing_time: 125,
-            results: resultJson,
-        }).id(`create:crushing/kubejs/${cleanID}`);
+        if (recipe.getOriginalRecipeIngredients().size() == 2) {
+            let ingredientsJson = [];
+            recipe.getOriginalRecipeIngredients().forEach(i => {
+                if (!i.test(GRINDER_ITEM)) {
+                    ingredientsJson.push(i.toJson());
+                }
+            });
+            let resultJson = [{
+                count: recipe.originalRecipeResult.count * 3,
+                id: recipe.originalRecipeResult.id
+            }];
+            let cleanID = recipe.originalRecipeResult.id.replace(':', '_');
+            event.custom({
+                type: "create:crushing",
+                ingredients: ingredientsJson,
+                processing_time: 125,
+                results: resultJson,
+            }).id(`create:crushing/kubejs/${cleanID}`);
+        }
     });
 });
